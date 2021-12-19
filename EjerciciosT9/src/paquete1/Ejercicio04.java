cf5fd9c848abab24dc1934dad26e395178cbf946
Ronin Carrion <rdcarrion3@utpl.edu.ec>
2021-12-18 21:19:58 -0500

    Taller9RoninCarrion

--- EjerciciosT9/src/paquete1/Ejercicio04.java
+++ EjerciciosT9/src/paquete1/Ejercicio04.java
@@ -15,13 +15,29 @@ public class Ejercicio04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        String cadena_acumuladora = " ";
         double[] promedios = {10, 10, 9.1, 7, 6.1, 4, 8};
-        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan", 
+        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan",
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
         String[] promediosCualitativos = new String[7];
-        
+        for (int indice = 0; indice < promedios.length; indice++) {
+            if (promedios[indice] >= 0 && promedios[indice] <= 5.9) {
+                promediosCualitativos[indice] = "Promedio Regular";
+            } else {
+                if (promedios[indice] >= 6 && promedios[indice] <= 8.9) {
+                    promediosCualitativos[indice] = "Promedio Bueno";
+                } else {
+                    promediosCualitativos[indice] = "Promedio Sobresaliente";
+                }
+            }
+            cadena_acumuladora = String.format("%s%s promedio: %.2f %s\n",
+                     cadena_acumuladora,
+                     estudiantes[indice],
+                     promedios[indice],
+                     promediosCualitativos[indice]);
+        }
+        System.out.println(cadena_acumuladora);
     }
 
 }
--- EjerciciosT9/src/paquete1/Ejercicio05.java
+++ EjerciciosT9/src/paquete1/Ejercicio05.java
@@ -21,20 +21,27 @@ public class Ejercicio05 {
         Antes de analizar el presente ejercicio,
         usted debe revisar y entender lo desarrollado
         en el archivo Ayuda01.java del paquete2
-        */
+         */
         Scanner entrada = new Scanner(System.in);
         String[] estudiantes = {"Kimberly", "Hogan",
             "Teresa", "Luis", "Mark", "Jennifer",
             "Alcides"};
         String inicial;
+        char inicial_convertida; // Por si el Usuario me ingresa una cadena
+                          // se tomará solamente el primer caracter
         char inicialLetra;
         boolean bandera = true;
         while (bandera) {
             System.out.println("Ingrese una letra");
             inicial = entrada.nextLine();
-            
+            inicial_convertida = inicial.charAt(0);
+            for(int i = 0; i < estudiantes.length; i++){
+                inicialLetra = estudiantes[i].charAt(0);
+                if(inicial_convertida == inicialLetra){
+                    bandera = false;
+                }
+            }
         }
-
     }
 
 }
